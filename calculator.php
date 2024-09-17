8a9ce5dd2286d1c668dad5037f6da1f13db135bb
EldridgeDaviss <eldridge.davis@students.jmcss.org>
2024-09-17 08:30:04 -0500

    Step 9
    Step 9

--- calculator.php
+++ calculator.php
@@ -3,10 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+
+    
     <title>Calculator</title>
 </head>
 <body>
-
+<?php     
+include 'includes/header.php';
+?>
+<br>
 <form action="">
     <input type="text" name="num1" placeholder= "num1 "id="">
     <input type="text" name="num2" placeholder= "num2 "id="">
@@ -18,11 +26,15 @@
         <option>Divide</option>
     </select>
     <br>
+   
+    <br>
     <button type="Submit" name="submit" value="submit">Calculate</button>
     </form>
 </form>
 <p>The answer is:</p>
   <?php
+    
+
   if (isset($_GET['submit'])) {
       $result1 = $_GET['num1'];
       $result2 = $_GET['num2'];
