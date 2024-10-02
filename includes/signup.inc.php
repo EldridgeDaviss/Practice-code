cd5dc9754971874dd553acbc0725b46e6882f156
EldridgeDaviss <eldridge.davis@students.jmcss.org>
2024-10-02 08:29:16 -0500

    finished signup

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -5,10 +5,10 @@ require 'dbh.inc.php';
 
 $username = $_POST['uid'];
 $email = $_POST['mail'];
-$pwd = $_POST['pwd'];
+$password = $_POST['pwd'];
 $passwordRepeat = $_POST['pwd-repeat'];
 
-if (empty($username) || empty($email) || empty($pwd) || empty($passwordRepeat)) { 
+if (empty($username) || empty($email) || empty($password) || empty($passwordRepeat)) { 
     header("Location: ../signup.php?error=emptyfields&uid=".$username."&mail=".$email);
     exit();
 
@@ -39,11 +39,44 @@ if (!mysqli_stmt_prepare($stmt, $sql)) {
   exit();
   }
  else {
-  mysqli_stmt_bind_param();
+  mysqli_stmt_bind_param($stmt, "s", $username);
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_store_result($stmt);
+  $resultCheck = mysqli_stmt_num_rows($stmt);
+  if ($resultCheck > 0) {
+    header("Location: ../signup.php?error=usertaken&mail=".$email);
+    exit();
+  }
+  else {
+
+    $sql = "INSERT INTO users (uidUsers, emailUsers, PwdUsers) VALUES (?, ?, ?)";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+      header("Location: ../signup.php?error=sqlerror");
+      exit();
+    }
+else {
+$hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+
+  mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
+  mysqli_stmt_execute($stmt);
+  header("Location: ../signup.php?signup=success");
+  exit();
+}
+  }
+
+
+
  }
-}
-
 
 }
+mysqli_stmt_close($stmt);
+mysqli_close($conn);
 
+
+}
+else {
+  header("Location: ../signup.php");
+  exit();
+}
 
