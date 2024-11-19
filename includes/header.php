ce898ac15531d809966c81dc10319e849fc5521a
EldridgeDaviss <eldridge.davis@students.jmcss.org>
2024-11-19 08:33:49 -0600

    Header ED

--- includes/header.php
+++ includes/header.php
@@ -10,70 +10,87 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>10 Feral Hogs</title>
 </head>
-<body>
-      
 
-<nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary">
+
+
+<body style="font-family: Arial, sans-serif; background-color: #f8f9fa;">
+
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark" style="background-color: #382102;">
   <div class="container-fluid">
-  <a class="navbar-brand" href="#">
-      <img src="img/Logo1.jpg" alt="Bootstrap" width="30" height="24">
+    <a class="navbar-brand d-flex align-items-center" href="#">
+      <img src="img/Logo1.jpg" alt="Logo" width="40" height="40" class="d-inline-block align-top me-2">
+      <span class="fw-bold">10 Feral Hogs</span>
     </a>
-  
-    <div class="collapse navbar-collapse" id="navbarSupportedContent" style="background-color:#382102;">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0" >
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php" style="color: white;">Home</a>
+          <a class="nav-link text-white" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="hogs.php" style="color: white;">Hogs</a>
+          <a class="nav-link text-white" href="hogs.php">Hogs</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="checkout.php" style="color: white;">Checkout</a>
+          <a class="nav-link text-white" href="checkout.php">Checkout</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="Hoglore.php" style="color: white;">Hog Lore</a>
+          <a class="nav-link text-white" href="Hoglore.php">Hog Lore</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="legalities.php" style="color: white;">Legalities</a>
+          <a class="nav-link text-white" href="legalities.php">Legalities</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="aboutus.php" style="color: white;">About us</a>
+          <a class="nav-link text-white" href="aboutus.php">About Us</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="contact.php" style="color: white;">Contact</a>
+          <a class="nav-link text-white" href="contact.php">Contact</a>
         </li>
       </ul>
-      <?php
-
- if (isset($_SESSION['userId'])) {
-  echo' <form action="includes/logout.inc.php" method="post">
-    <button type="submit" name="logout-submit">Logout</button>
-  </form>';
-   }
-  else {
-      echo'  <form action="includes/login.inc.php" method="post">
-    <input type="text" name="mailuid" placeholder="Username/E-mail...">
-    <input type="password" name="pwd" placeholder="Password...">
-    <button type="submit" name="login-submit">Login</button>
-  </form>
-  &nbsp;&nbsp;
-  <a href="signup.php">Signup</a>';
-  }
-
-?>
+      <div class="d-flex">
+        <?php
+          if (isset($_SESSION['userId'])) {
+            echo '<form action="includes/logout.inc.php" method="post" class="d-flex align-items-center">
+                    <button class="btn btn-outline-light btn-sm" type="submit" name="logout-submit">Logout</button>
+                  </form>';
+          } else {
+            echo '<button class="btn btn-outline-light btn-sm me-2" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
+                  <a href="signup.php" class="btn btn-light btn-sm">Signup</a>';
+          }
+        ?>
+      </div>
     </div>
   </div>
 </nav>
-  
 
-
-
-<div>
-
- 
+<!-- Login Modal -->
+<div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="loginModalLabel">Login</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="includes/login.inc.php" method="post">
+          <div class="mb-3">
+            <label for="mailuid" class="form-label">Username/E-mail</label>
+            <input type="text" class="form-control" id="mailuid" name="mailuid" placeholder="Enter your username or email">
+          </div>
+          <div class="mb-3">
+            <label for="pwd" class="form-label">Password</label>
+            <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Enter your password">
+          </div>
+          <button type="submit" name="login-submit" class="btn btn-primary w-100">Login</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
+
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -30,7 +30,8 @@
       <link rel="stylesheet" href="css/owl.theme.default.min.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
 
-
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
       <style>
         body {
             cursor: url('images/custom-cursor.png'), auto;
@@ -243,8 +244,14 @@
                   <div class="col-lg-4 col-sm-12 col-md-4">
                      <div class="box_main active">
                         <div class="house_icon">
-                           <img src="images/icon1.png" class="image_1">
-                           <img src="images/icon1.png" class="image_2">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" fill="currentColor" class="bi bi-cash-coin" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8m5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0"/>
+  <path d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195z"/>
+  <path d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083q.088-.517.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1z"/>
+  <path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 6 6 0 0 1 3.13-1.567"/>
+</svg>
+
+                         
                         </div>
                         <h3 class="decorate_text">Selling Hogs</h3>
                         <p class="tation_text"> Our expertly trained hogs are sold in batches of ten, ensuring quality and consistency for every purpose.</p>
@@ -254,8 +261,11 @@
                   <div class="col-lg-4 col-sm-12 col-md-4">
                      <div class="box_main">
                         <div class="house_icon">
-                           <img src="images/icon2.png" class="image_1">
-                           <img src="images/icon2.png" class="image_2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" fill="currentColor" class="bi bi-clipboard2-check-fill" viewBox="0 0 16 16">
+  <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5"/>
+  <path d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585q.084.236.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5q.001-.264.085-.5m6.769 6.854-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708.708"/>
+</svg>
+                           
                         </div>
                         <h3 class="decorate_text">Training Hogs</h3>
                         <p class="tation_text">At 10 Feral Hogs, we specialize in training hogs to excel in unique roles, from vacation companions to law enforcement partners.</p>
@@ -265,8 +275,10 @@
                   <div class="col-lg-4 col-sm-12 col-md-4">
                      <div class="box_main">
                         <div class="house_icon">
-                           <img src="images/icon3.png" class="image_1">
-                           <img src="images/icon3.png" class="image_2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="110" height="110" fill="currentColor" class="bi bi-globe-americas" viewBox="0 0 16 16">
+  <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0M2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484q-.121.12-.242.234c-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z"/>
+</svg>
+                          
                         </div>
                         <h3 class="decorate_text">Feral Hog Collection</h3>
                         <p class="tation_text">We carefully collect wild feral hogs and transform them into well-trained, versatile animals ready to meet our customers’ needs.</p>
