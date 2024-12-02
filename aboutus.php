37cb4d6e614cd67da362717e4606b84dfee53a8e
EldridgeDaviss <eldridge.davis@students.jmcss.org>
2024-12-02 08:42:37 -0600

    About Change ED

--- aboutus.php
+++ aboutus.php
@@ -1,63 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<link rel="icon" type="image/x-icon" href="img/Hog photos/hogfavicon.ico">
+    <link rel="icon" type="image/x-icon" href="img/Hog photos/hogfavicon.ico">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="about.css">
-    <?php
-    include 'includes/header.php';
-    ?>
-   <br>
-    <title>Home</title>
+    <?php include 'includes/header.php'; ?>
+    <title>About Us</title>
 </head>
+
 <body>
+    <!-- Intro Section -->
+    <section class="text-center py-5" style="background-color: #fdfdfd;">
+        <h1 class="mb-3">About Us</h1>
+        <p class="text-muted">Discover our mission, values, and the faces behind 10 Feral Hogs.</p>
+    </section>
 
-<p id="ptext">
-</p>
+    <!-- Mission Statements Section -->
+    <section class="container my-5">
+        <div class="row g-4">
+            <!-- Statement 1 -->
+            <div class="col-md-4">
+                <div class="card h-100">
+                    <img src="images/profile.jpg" class="card-img-top rounded-circle mx-auto mt-3" alt="Samuel Latham" style="width: 100px; height: 100px; object-fit: cover;">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Samuel Latham</h5>
+                        <p class="card-text">
+                            "There is no greater joy than that of the hero. At 10 Feral Hogs, we strive to not only bask in that joy but bring it to our customers. Our product brings success, not only in attaining that goal but also in being the better person."
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- Statement 2 -->
+            <div class="col-md-4">
+                <div class="card h-100">
+                    <img src="images/profile.jpg" class="card-img-top rounded-circle mx-auto mt-3" alt="Mason Blankenship" style="width: 100px; height: 100px; object-fit: cover;">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Mason Blankenship</h5>
+                        <p class="card-text">
+                            "Ever since I was a boy, I've wanted to work with animals and spread that love to others. No animal is more misunderstood than the common feral hog. We aim to change the public's thoughts from pests to intelligent creatures deserving love."
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- Statement 3 -->
+            <div class="col-md-4">
+                <div class="card h-100">
+                    <img src="images/profile.jpg" class="card-img-top rounded-circle mx-auto mt-3" alt="Eldridge Davis" style="width: 100px; height: 100px; object-fit: cover;">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Eldridge Davis</h5>
+                        <p class="card-text">
+                            "Our mission is to humanely transform wild hogs into well-adjusted animals through compassionate training and sustainable practices, turning challenges into opportunities for positive ecosystem change."
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
+    <!-- Call to Action -->
+    <section class="text-center py-5" style="background-color: #f8f9fa;">
+        <h2 class="mb-4">Ready to Make a Difference?</h2>
+        <p class="mb-4">Join us in reshaping the world, one hog at a time.</p>
+        <a href="hogs.php" class="btn btn-primary btn-lg">Shop With Us!</a>
+    </section>
 
-
-
-<img src="img/Hog photos/OIP(4).jpg" class="aimage" alt="Hog Image">
-<p class="atext">About Us</p>
-<div class="acover"></div>
-
-<div class="blackspace">
-    <p class="btext">Who We Are</p>
-
-    <p class="parabtext">"At 10 Feral Hogs, our mission is to humanely transform wild hogs into well-adjusted animals <br>
-        through compassionate training, sustainable practices, and dedication to harmonious coexistence. <br>
-        We believe in bridging the gap between the wild and domestic worlds, turning <br>
-        challenges into opportunities for positive change in the ecosystem." <br>  ---Eldridge Davis  
-    </p>                        
-
-    <p class="para2btext">"There is no greater joy than that of the hero, and in all of our efforts we strive to not only <br>
-        bask in that joy but bring it to our customers. 10 Feral Hogs is a family, forged by a desire<br>
-        for greatness through tranquility. Our product brings success, not only in attaining that goal, <br>
-        but also in being the better person." <br>  ---Samuel Latham  
-    </p> 
-
-    <p class="para3btext">"Ever since I was a boy, I've always wanted to work with animals, and to spread that love of animals <br>
-        to others. This is the core principal 10 Feral Hogs is based around. No animal is more misunderstood than the common feral hog. We have put all our effort into changing<br>
-        the public's thoughts that these animals are pests, but instead intelligent creatures deserving of love.<br>
-        And we will not stop until we have succeeded at that goal." <br>  ---Mason Blankenship  
-    </p> 
-
-    <a href="hogs.php" class="shopw"> 
-        <button class="button">Shop With Us!</button>
-    </a>
-</div>
-
-
-
-<br>
-<?php
-include 'includes/footer.php'
-
-?>
+    <!-- Footer -->
+    <?php include 'includes/footer.php'; ?>
 </body>
+
 </html>
